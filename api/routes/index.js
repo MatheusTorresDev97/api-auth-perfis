f99a0f456d519fdc1332d2ed1f9bc38103d53177
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-11-22 09:49:15 -0300

    Iniciando crud permissão

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,13 +4,15 @@ const produto = require('./produtoRoute')
 const usuario = require('./usuariosRoute')
 const auth = require('./authRoute')
 const role = require('./role')
+const permissao = require('./permissao')
 
 module.exports = app => {
   app.use(
     bodyParser.json(),
     auth,
-    usuario,
     produto,
-    role
+    usuario,
+    role,
+    permissao
   )
 }
---
+++ api/routes/permisssao.js
@@ -0,0 +1,13 @@
+const { Router } = require('express')
+
+const router = Router()
+
+router
+    .post('/permissao')
+    .get('/permissao')
+    .get('/permissao/:id')
+    .delete('/permissao/:id')
+    .put('/permissao/:id')
+
+
+    module.exports = router
