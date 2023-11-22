ab0726f665a25d13ada655dedfbb43cfb1b34e8b
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-11-22 09:33:06 -0300

    criando rotas de role

--- api/routes/role.js
+++ api/routes/role.js
@@ -1,9 +1,10 @@
 const { Router } = require('express')
+const RoleController = require('../controllers/roleController')
 
 const router = Router()
 
 router
-    .post('/role')
+    .post('/roles', RoleController.cadastrar)
     .get('/role')
     .get('/role/:id')
     .delete('/role/:id')
