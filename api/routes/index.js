8774d37d4044f6590bb336093aecce42248ef5a1
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-11-22 10:25:34 -0300

    implementando relacionamento entre tabelas

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,7 +4,7 @@ const produto = require('./produtoRoute')
 const usuario = require('./usuariosRoute')
 const auth = require('./authRoute')
 const role = require('./role')
-const permissao = require('./permissao')
+const permissao = require('./permisssao')
 
 module.exports = app => {
   app.use(
